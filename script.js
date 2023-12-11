f814e0210bf78a39e23cc4c7431b1d5a1a3e5b8d
Echo <sne.rtp1@gmail.com>
2023-12-11 18:02:40 +0300

    fixed some bugs, added new block

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 const degree = document.querySelector('.main__degree');
 const city = document.querySelector('.main__city');
 const img = document.querySelector('.main__img');
+const feels = document.getElementById('feels');
+
 const cloud = 'img/cloud.png';
 const snow = 'img/snowflake.png';
 const clear = 'img/sun.png';
@@ -17,91 +19,21 @@ const rain = 'img/rain.png';
 
 let dataBase = [];
 
-// функция, изменяющая информацию погоды
-const editElement = (town, degrees, weather) => {
-    city.textContent = town;
-    degree.textContent = degrees;
-    img.src = weather
-}
-
-// функция добавляющая элемент в список избранных
-const addElement = (element , city, nameOfClass) => {
-    const newElement = document.createElement(element);
-    newElement.className = nameOfClass;
-    newElement.textContent = city;
-    return newElement
-
-
-}
-
-
-const toShowElement = () => {
-    const listName = document.querySelectorAll('.main__li');
-
-    const showElement = (event) => {
-        event.preventDefault();
-        const getText = event.target.textContent;
-        getWeatherOptinons(getText);
-
-    }
-
-    listName.forEach((item) => {
-        item.addEventListener('click', showElement);
-    })
-}
-
-const toDeleteElement = () => {
-    const closeElement = document.querySelectorAll('.main__close');
-
-    closeElement.forEach((item, i) => {
-        item.addEventListener('click', (parent) => {
-            parent.preventDefault();
-            const block = parent.target.parentNode;
-            dataBase.splice(i - 1, 1);
-            block.remove()
-        });
-    })
-
-    
-}
-
-
-
-const toMakeNewElement = () => {
-    const block = addElement('div', '', "main__cities");
-    ul.appendChild(block);
-
-    const nameOfCity = addElement('li', toFirstUppercase(input.value), 'main__li');
-    block.appendChild(nameOfCity)
-
-    const close = addElement('div', '', 'main__close')
-    block.appendChild(close);
-
-   toDeleteElement();
-   toShowElement()
-}
-
-const addLocation = (event) => {
+// когда пользователь вводит данные, вызывается данная функция
+const addWeather = (event) => {
     event.preventDefault();
-    
-    for (let i = 0; i < dataBase.length; i++) {
-        if (dataBase[i] === input.value) return
-    }
-    
-    dataBase[dataBase.length] = input.value;
-    toMakeNewElement();  
-    
+    getWeatherOptinons(input.value);
+   
 }
 
-const toFirstUppercase = (elem) => {
-    return elem[0].toUpperCase() + elem.slice(1);
-}
 
+// функция которая вывводит данные 
 const getWeatherOptinons = (value) => {
     fetch(`${serverUrl}?q=${value}&appid=${apiKey}`)
         .then(result => result.json())
         .then(data => {
             let elements;
+            console.log(data.main.feels_like - 273);
             switch(data.weather[0].main) {
                 case 'Clouds':
                     elements = cloud;
@@ -118,19 +50,94 @@ const getWeatherOptinons = (value) => {
                 case 0:
                     throw new Error('Введен не существующий поселок')
             }
-            editElement(toFirstUppercase(value), Math.round(data.main.temp - 273), elements);
+            editElement(toFirstUppercase(value), Math.round(data.main.temp - 273), elements, Math.round(data.main.feels_like - 273));
             
         });
 }
 
-const addWeather = (event) => {
-    event.preventDefault();
-    getWeatherOptinons(input.value);
-    heart.addEventListener('click', addLocation)
+
+
+
+// функция, изменяющая информацию погоды
+const editElement = (town, degrees, weather, feelings) => {
+    city.textContent = town;
+    degree.textContent = degrees;
+    img.src = weather
+    feels.textContent = feelings;
 }
- 
 
+// при клике на сердечко происходит проверка города, если нету в массиве, то он добавляется
+const addLocation = (event) => {
+    event.preventDefault();
+    console.log(dataBase);
+    
+    if (dataBase.includes(input.value) === true) return
+    
+    
+    dataBase[dataBase.length] = input.value;
+    toMakeNewElement();  
+    
+}
 
+// добавляет в список добавленных городов
+const toMakeNewElement = () => {
+    const block = addElement('div', '', "main__cities");
+    ul.appendChild(block);
 
+    const nameOfCity = addElement('li', toFirstUppercase(input.value), 'main__li');
+    block.appendChild(nameOfCity)
+
+    const close = addElement('div', '', 'main__close')
+    block.appendChild(close);
+
+    toDeleteElement();
+    toShowElement()
+}
+
+//показывает элемент при клике на него
+const toShowElement = () => {
+    const listName = document.querySelectorAll('.main__li');
+
+    const showElement = (event) => {
+        event.preventDefault();
+        const getText = event.target.textContent;
+        getWeatherOptinons(getText);
+    }
+
+    listName.forEach((item) => {
+        item.addEventListener('click', showElement);
+    })
+}
+
+// удаляет элемент из списка избранных
+const toDeleteElement = () => {
+    const closeElement = document.querySelectorAll('.main__close');
+
+    closeElement.forEach((item, i) => {
+        item.addEventListener('click', (parent) => {
+                parent.preventDefault();
+                const block = parent.target.parentNode;
+                dataBase.splice(i, 1);
+                console.log(dataBase); 
+                block.remove()
+        });
+
+    })
+    
+}
+
+// функция добавляющая элемент в список избранных
+const addElement = (element , city, nameOfClass) => {
+    const newElement = document.createElement(element);
+    newElement.className = nameOfClass;
+    newElement.textContent = city;
+    return newElement
+}
+
+// делает первую букву заглавной
+const toFirstUppercase = (elem) => {
+    return elem[0].toUpperCase() + elem.slice(1);
+}
+
+heart.addEventListener('click', addLocation)
 form.addEventListener('submit', addWeather);
-
