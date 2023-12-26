91c47a5ad99c4878a885ab1fc4f32a7c915ffd15
Echo <sne.rtp1@gmail.com>
2023-12-26 15:21:45 +0200

    add async

--- js/main.js
+++ js/main.js
@@ -12,26 +12,21 @@ const addWeather = (event) => {
     
 }
 
-export const getWeatherOptions = (value) => {
-    fetch(`${API.SERVER_URL}?q=${value}&appid=${API.API_KEY}`)
+export async function getWeatherOptions (value) {
+    const responce = await fetch(`${API.SERVER_URL}?q=${value}&appid=${API.API_KEY}`)
+    console.log(responce);
+    switch (responce.status) {
+        case 400:
+            alert(ERROR.ENTER_NAME);
+            break;
+        case 404:
+            alert(ERROR.CORRECT_NAME);
+            break;
+    }
+    const data = await responce.json();    
+    editElement(toFirstUppercase(value), Math.round(data.list[0].main.temp - 273), editImg(data.list[0].weather[0].main), Math.round(data.list[0].main.feels_like - 273), getDate(data.city.sunrise), getDate(data.city.sunset));
+    editFutureElements(data);
         
-        .then(result => {
-            switch (result.status) {
-                case 400:
-                    alert(ERROR.ENTER_NAME);
-                    break;
-                case 404:
-                    alert(ERROR.CORRECT_NAME);
-                    break;
-                default:
-                    return result.json()
-
-            }
-        })
-        .then(data => {
-            editElement(toFirstUppercase(value), Math.round(data.list[0].main.temp - 273), editImg(data.list[0].weather[0].main), Math.round(data.list[0].main.feels_like - 273), getDate(data.city.sunrise), getDate(data.city.sunset));
-            editFutureElements(data);
-        });
         
 }
 
